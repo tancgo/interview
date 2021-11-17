4c722b27ae0f2337a2535349d45cf328a3a28beb
kaze <542112893@qq.com>
2021-11-17 14:55:52 +0800

    feat: bubble sort

---
+++ sort/bubble.js
@@ -0,0 +1,15 @@
+var sortArray = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+      }
+    }
+  }
+
+  return nums;
+};
+
+const nums = [5,1,1,2,0,0]
+
+console.log(sortArray(nums));
