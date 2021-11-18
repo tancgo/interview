2b5ee8bf199bcb8ce113228bbedcf341f0a9147f
kaze <542112893@qq.com>
2021-11-18 11:15:31 +0800

    feat: concurrent control

--- promise/concurrent.js
+++ promise/concurrent.js
@@ -15,7 +15,6 @@ async function concurrentControl(limit, requests) {
     // 当limit值小于或等于总任务个数时，进行并发控制
     if (limit <= requests.length) {
       console.log(res, "res");
-
       // 当任务完成后，从正在执行的任务数组中移除已完成的任务
       const e = p.then(() => executing.splice(executing.indexOf(e), 1));
       // 保存正在执行的异步任务
@@ -26,26 +25,26 @@ async function concurrentControl(limit, requests) {
       }
     }
   }
-
+  console.log(res, "res end");
   return Promise.all(res);
 }
 
-let i = 0;
-function generateRequest() {
-  const j = ++i;
+function generateRequest(j) {
   return function request() {
     return new Promise((resolve) => {
-      console.log(`r${j}...`);
       setTimeout(() => {
         resolve(`r${j}`);
       }, 1000 * j);
     });
   };
 }
-const requests = new Array(6).fill("").map(() => generateRequest());
+
+const requests = new Array(6)
+  .fill("")
+  .map((_, index) => generateRequest(index));
 
 async function main() {
-  const results = await concurrentControl(2, requests);
+  const results = await concurrentControl(3, requests);
   console.log(results);
 }
 main();
