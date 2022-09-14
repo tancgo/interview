526880877eb5c256e67e8f9c546174fc84120104
tan <tan@tandeMBP.ctc>
2022-09-14 15:03:52 +0800

    feat: hex2rgb

---
+++ hex2rgb.js
@@ -0,0 +1,26 @@
+function hex2rgb(hex) {
+  if (typeof hex !== "string") return;
+  hex = hex.toLowerCase();
+  const reg = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+  if (hex && reg.test(hex)) {
+    // 判断是否是简写
+    if (hex.length === 4) {
+      let newHex = "#";
+      for (let i = 1; i < 4; i++) {
+        newHex = newHex + hex[i] + hex[i];
+      }
+      hex = newHex;
+    }
+
+    const arr = [];
+    for (let i = 1; i < 7; i += 2) {
+      arr.push(parseInt(hex.slice(i, i + 2), 16));
+    }
+    console.log(`rgb(${arr.join(',')})`);
+    return `rgb(${arr.join(',')})`
+  }
+  return hex;
+}
+
+hex2rgb("#000000");
+hex2rgb("#CC00FF");
